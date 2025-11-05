528ceba7119afd57a7f6457901b869644fe9017e
Tharindu <tharinduonline1080@gmail.com>
2025-11-05 17:24:26 +0530

    implemented api fuctionality: getting longitude and latitude using city name.

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search } from "lucide-react";
 
-export default function SearchBox() {
+export default function SearchBox({ onSearchLocation }) {
+  const [location, setLocation] = useState();
+
+  const setLoactionInput = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const clickSearch = () => {
+    if (location.trim() !== "") {
+      onSearchLocation(location);
+      setLocation("");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      clickSearch();
+    }
+  };
+
   return (
     <div className="flex flex-col items-center text-white">
       <h1 className="text-4xl font-bold mb-6">Weather App</h1>
@@ -12,12 +31,16 @@ export default function SearchBox() {
         <input
           type="text"
           placeholder="Search location"
+          value={location}
+          onChange={setLoactionInput}
+          onKeyPress={handleKeyPress}
           className="flex-1 h-full px-5 text-lg bg-transparent text-gray-200 placeholder-gray-400 focus:outline-none"
         />
 
         {/* Larger search button */}
         <button
           type="button"
+          onClick={clickSearch}
           className="w-20 h-full flex items-center justify-center bg-[#3B82F6] hover:bg-[#2563EB] transition-all"
         >
           <Search className="w-5 h-5 text-white" />
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBox from "../../components/SearchBox";
 import WeatherCard from "../../components/WeatherCard";
+import axios from "axios";
 
 export default function HomePage() {
+  const [geoRes, setGeoRes] = useState();
+  const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
+  const searchLocation = (locationText) => {
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${locationText}&limit=5&appid=${apiKey}`
+      )
+      .then((res) => {
+        setGeoRes(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.error("Error fetching geo location:", err));
+  };
+
+  useEffect(() => {
+    // call open weather api with lat , lon and get weather data
+  }, [geoRes]);
+
   return (
     <div className="bg-[#1F2937] min-h-screen flex flex-col items-center justify-center gap-10 py-10">
-      <SearchBox />
+      <SearchBox onSearchLocation={searchLocation} />
       <WeatherCard />
     </div>
   );
